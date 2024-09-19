aa95d3f53dcfe1c521b14a517d5c5fa6b6d8f6ee
Cuong0685123 <eticon0685@gmail.com>
2024-09-19 16:01:14 +0700

    update all modelHotel

--- src/main/java/com/tc/booking/model/entity/Hotel.java
+++ src/main/java/com/tc/booking/model/entity/Hotel.java
@@ -19,17 +19,19 @@ public class Hotel {
     private String name;
     private String address;
     private float rating;
+    private String image;
 
    
     @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER) // Changed to EAGER
     private List<Room> rooms;
     public Hotel() { super(); }
 
-    public Hotel(int id, String name, String address, float rating, List<Room> rooms) {
+    public Hotel(int id, String name, String address, float rating, List<Room> rooms, String image) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.rating = rating;
         this.rooms = rooms;
+        this.image = image;
     }
 }
